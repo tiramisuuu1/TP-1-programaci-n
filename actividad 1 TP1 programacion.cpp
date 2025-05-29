7274075e777ac1db7faa2b679f4acc962e95ca4d
tiramisu <polentita9@gmail.com>
2025-05-29 08:11:44 -0300

    Add files via upload

---
+++ actividad 1 TP1 programacion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int n1, n2, resultado1=0, resultado2=0;
+
+
+int main()
+{
+
+cout<<"ingrese dos numeros"<<endl; 
+cin>>n1;cin>>n2;
+
+
+resultado1 = n1 + n2;
+resultado2 = n1 * n2;
+
+cout<<"el resultado de la suma es: "<<resultado1;
+cout<<"y el resultado de la multiplicacion es: "<<resultado2;
+
+return 0;
+}
